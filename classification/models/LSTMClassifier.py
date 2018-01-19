a3ce4cbcd766aa86c0f2fc97887bab4124062283
horizon94 <horizon94@outlook.com>
2018-01-19 16:10:23 +0800

    1

--- classification/models/LSTMClassifier.py
+++ classification/models/LSTMClassifier.py
@@ -1,17 +1,40 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
-class LSTMClassifier(nn.Module):
-    def __init__(self,opt):
-        super(LSTMClassifier, self).__init__()
-        self.opt=opt
-        self.embed_dim=opt.embed_dim
-        self.hidden_dim=opt.hidden_dim
-        self.batch_size=opt.batch_size
-        self.use_gpu=torch.cuda.is_available()
+import torch
+from torch.autograd import Variable
+from torch.nn import Parameter
 
-        self.embeddingLayer=nn.Embedding(opt.vocab_size,self.embed_dim)
-        self.lstm=nn.LSTM(self.embed_dim,self.hidden_dim)
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        return F.relu(self.conv2(x))
+
+class LSTMClassifier(nn.Module):
+
+    def __init__(self, args, embedding_loader = None):
+        super(LSTMClassifier, self).__init__()
+        self.hidden_dim = args.hidden_dim
+        self.batch_size = args.batch_size
+        self.use_gpu = args.use_gpu
+
+        self.word_embeddings = nn.Embedding(embedding_loader.embeddings.shape[0], args.embedding_dim)
+        self.lstm = nn.LSTM(args.embedding_dim, args.hidden_dim)
+        self.hidden2label = nn.Linear(args.hidden_dim, args.articles_num)
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        if self.use_gpu:
+            self.h0 = Variable(torch.zeros(1, self.batch_size, self.hidden_dim).cuda())
+            self.c0 = Variable(torch.zeros(1, self.batch_size, self.hidden_dim).cuda())
+        else:
+            self.h0 = Variable(torch.zeros(1, self.batch_size, self.hidden_dim))
+            self.c0 = Variable(torch.zeros(1, self.batch_size, self.hidden_dim))
+        return (self.h0, self.c0)
+
+    def forward(self, sentence):
+
+        embeds = self.word_embeddings(sentence)
+        x = embeds#.view(len(sentence), self.batch_size, -1)
+        lstm_out, (h_n,c_n) = self.lstm(x, self.hidden)
+        # print(lstm_out.shape,h_n.shape,c_n.shape)
+        # print(lstm_out[-1][0])
+        # print(h_n[0][0])
+        # print("*"*50)
+        y  = self.hidden2label(h_n[-1])
+        return y
